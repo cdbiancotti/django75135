bf5990dd04cd8a34b47bfe74ee844718135e3937
Cristian D Biancotti <cdbiancotti@gmail.com>
2025-02-11 20:53:30 -0300

    Todo lo requerido para la entrega 3

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,9 +1,11 @@
-from inicio.views import inicio, saludo
+from inicio.views import inicio, saludo, crear_auto, listado_de_autos
 # from .views import inicio
 
 from django.urls import path
 
 urlpatterns = [
     path('', inicio, name='inicio'),
-    path('saludo/<str:nombre>/<str:apellido>/', saludo, name='saludo')
+    path('saludo/<str:nombre>/<str:apellido>/', saludo, name='saludo'),
+    path('crear-auto/', crear_auto, name='crear_auto'),
+    path('listado-de-autos/', listado_de_autos, name='listado_de_autos'),
 ]
